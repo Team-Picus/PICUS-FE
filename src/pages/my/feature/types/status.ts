b5456b6f30de3235896909853864df210a5802c1
Tnalxmsk <alstnwkd990@naver.com>
2025-03-25 17:54:58 +0900

    :sparkles: feat: 작가 상태 정의

---
+++ src/pages/my/feature/types/status.ts
@@ -0,0 +1,14 @@
+export enum ExpertStatusType {
+  COMPLETE = '완료',
+  REJECT = '거절',
+  REVIEW = '심사중',
+  NONE = '미신청',
+}
+
+// API 스팩 확인 후 제거 or 변경
+/*const ExpertStatusType: Record<string, string> = {
+  COMPLETE: '완료',
+  REJECT: '거절',
+  REVIEW: '심사중',
+  NONE: '미신청',
+};*/
