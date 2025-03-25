16563b94ae61118058f3fea794b08adc89fb2d41
Tnalxmsk <alstnwkd990@naver.com>
2025-03-25 17:53:34 +0900

    :art: style: 내 정보 컴포넌트 구현

---
+++ src/pages/my/components/MyInfo.tsx
@@ -0,0 +1,16 @@
+const MyInfo = () => {
+  return (
+    <div>
+      <div>
+        <img />
+        <div>
+          <p>이름</p>
+          <p>이메일</p>
+        </div>
+      </div>
+      <img />
+    </div>
+  );
+};
+
+export default MyInfo;
