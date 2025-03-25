f0099c4189519a63524d66e702f96f290dae4222
Tnalxmsk <alstnwkd990@naver.com>
2025-03-25 14:42:39 +0900

    :sparkles: feat: MyPage rotuer

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -2,7 +2,8 @@ import { createBrowserRouter } from 'react-router';
 import { RootLayout } from '@app/layout';
 import { HomePage } from '@pages/home';
 import { ExpertMainPage } from '@pages/expert/ui/ExpertMainPage.tsx';
-import { ExpertDetailPage } from "@pages/my";
+import { ExpertDetailPage } from 'pages/profile';
+import { MyPage } from '@pages/my';
 
 export const router = createBrowserRouter([
   {
@@ -26,12 +27,22 @@ export const router = createBrowserRouter([
     ],
   },
   {
-    path: '/my',
+    path: '/profile/:expertId',
     element: <RootLayout />,
     children: [
       {
         index: true,
         element: <ExpertDetailPage />,
+      },
+    ],
+  },
+  {
+    path: '/my',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <MyPage />,
       },
     ],
   },
